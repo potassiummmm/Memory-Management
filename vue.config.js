22cf62c64b95df2ede396859b2c43932cbf86505
potassiummmm <54800242+potassiummmm@users.noreply.github.com>
2021-06-09 19:47:21 +0800

    update project

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/memory_management/'
+        ? '/Memory-Management/'
         : '/',
     // configureWebpack: {
     //     performance: {
