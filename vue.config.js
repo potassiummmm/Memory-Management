590ea61d13fa68723302308b1dc861a33d45202a
potassiummmm <54800242+potassiummmm@users.noreply.github.com>
2021-06-09 19:26:09 +0800

    update project

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/memory_management/'
+        : '/',
+    configureWebpack: {
+        performance: {
+            hints:'warning',
+            maxEntrypointSize: 50000000,
+            maxAssetSize: 30000000,
+            assetFilter: function(assetFilename) {
+                return assetFilename.endsWith('.js') || assetFilename.endsWith('.css');
+            }
+        }
+    }
+}
